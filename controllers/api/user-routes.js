ff1fdba0e5c799e9c5267da3a6e3c8c07df965ba
grilledcheeseplease <lacynunnally402@gmail.com>
2022-07-04 19:21:11 -0400

    finished user-routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,148 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+// GET all Users
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll({ 
+            attributes: { 
+                exclude: ['password'] 
+            }
+        });
+        res.status(200).json(userData); 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET a single User
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Post,
+                    attributes: {
+                        exclude: ['user_id']
+                    }
+                },
+                {
+                    model: Comment,
+                    attributes: {
+                        exclude: ['post_id', 'user_id']
+                    },
+                },
+            ],
+        });
+        
+        if (!userData) {
+            res.status(404).json({ message: 'No user with this id' });
+            return;
+        }
+
+        res.json(userData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// create User
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// User login
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { username: req.body.username } });
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// User logout
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+// User update
+router.put('/:id', async (req, res) => {
+    try {
+        const userData = await User.update(req.body, {
+            individualHooks: true,
+            where: { id: req.params.id}
+        });
+
+        if (!userData[0]) {
+            res.status(404).json({ message: 'No user with this id' });
+            return;
+        };
+
+        res.status(200).json(userData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// User DELETE 
+router.delete('/:id', async (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!userData) {
+            res.status(404).json({ message: 'No user with this id' });
+            return;
+        };
+
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
---
+++ utils/auth.js
@@ -0,0 +1,9 @@
+const withAuth = (req, res, next) => {
+    if(req.session.loggedIn) {
+      next();
+    } else {
+      res.redirect('/login');
+    }
+  };
+  
+  module.exports = withAuth;
